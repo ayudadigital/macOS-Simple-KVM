0371e161fd0314a2025f0a80f5dd4c62cf890ae2
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-05-24 15:04:20 +0000

    Upgrade: Use duing base instead ubuntu

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged --name=macindocker --restart=unless-stopped -dti -v $(pwd):$(pwd) -w $(pwd) -p 5900:5900 ayudadigital/macindocker
+docker run --privileged --name=macindocker --restart=unless-stopped -dti -v $(pwd):$(pwd) -w $(pwd) -v /dev/shm:/dev/shm -p 10022:22 -p 5900:5900 ayudadigital/macindocker:beta
